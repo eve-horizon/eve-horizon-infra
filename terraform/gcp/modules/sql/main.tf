d393f5417dc49b5177927e1fcbbe3cf38aec7109
TiGz <ajchesney@gmail.com>
2026-02-26 12:32:04 +0000

    backport: fix: add Workload Identity for backup CronJobs, cap transaction log retention at 7 days
    Source: corfai-eve-infra 0c88a04

--- terraform/gcp/modules/sql/main.tf
+++ terraform/gcp/modules/sql/main.tf
@@ -25,7 +25,7 @@ resource "google_sql_database_instance" "main" {
       enabled                        = true
       start_time                     = "03:00"
       point_in_time_recovery_enabled = true
-      transaction_log_retention_days = 14
+      transaction_log_retention_days = 7
 
       backup_retention_settings {
         retained_backups = 30
